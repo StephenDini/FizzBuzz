53e1ad5c3e0d6c86ca932095b250aecda6b83441
StephenDini <stephendini@gmail.com>
2019-09-06 14:39:36 -0400

    first commit

---
+++ fizzbuzzcpp/fizzbuzzcpp.cpp
@@ -0,0 +1,42 @@
+// fizzbuzzcpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //"Write a program that prints the numbers from 1 to 100. 
+	// But for multiples of three print “Fizz” instead of the number 
+	// and for the multiples of five print “Buzz”. For numbers which 
+	// are multiples of both three and five print “FizzBuzz”
+
+	for (int i = 1; i <= 100; i++)
+	{
+		cout << i << ": ";
+		if (i % 3 == 0 && i % 5 == 0)
+			cout << "FizzBuzz" << endl;
+		else if (i % 5 == 0)
+			cout << "Buzz" << endl;
+		else if (i % 3 == 0)
+			cout << "Fizz" << endl;
+		else
+			cout << i << endl; 
+	}
+	string enter;
+
+	cin >> enter;
+	return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
